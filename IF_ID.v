c9ea5972e164a397c72a9edbbc4febecce430ef5
Ericka Bastías 柏藝安 <b03902117@ntu.edu.tw>
2016-12-02 12:21:46 +0800

    if id

--- IF_ID.v
+++ IF_ID.v
@@ -17,12 +17,12 @@ module IF_ID
 	rd_o
 );
 
-input	[31:0]	inst_addr_i,inst_i,
-				inst_addr1_o,inst_addr2_o;
+input	[31:0]	inst_addr_i,inst_i;
 input			hd_i,flush_i;
 output	[25:0]	mux2_o;
 output	??		hd_o;
 output	[5:0]	op_o;
+output	[31:0]	inst_addr1_o,inst_addr2_o;
 output	[4:0]	rs1_o,rt1_o, rs2_o, rt2_o, rd_o;
 output	[15:0]	sign16_o;
 
