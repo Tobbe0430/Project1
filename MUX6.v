27fc3daa2d7cc89c064a8608a418a7d266037eb0
Ericka Bastías 柏藝安 <b03902117@ntu.edu.tw>
2016-12-06 23:01:11 +0800

    MUX6 and 7

--- MUX6.v
+++ MUX6.v
@@ -11,6 +11,18 @@ input	[31:0]	data1_i, data2_i, data3_i;
 input	[1:0]	fu_i;
 output	[32:0]	data_o;
 
+reg		temp_data_o;
+assign	data_o = temp_data_o;
 
+always @ (*)
+begin
+		case(fu_i)
+			2'b00:	temp_data_o = data1_i;
+			2'b01:	temp_data_o = data2_i;
+			2'b10:	temp_data_o = data3_i;
+			default:
+			
+endcase
+end
 
 endmodule
--- MUX7.v
+++ MUX7.v
@@ -12,4 +12,23 @@ input	[31:0]	data1_i, data2_i, data3_i;
 input	[1:0]	fu_i;
 output	[31:0]	data1_o, data2_o;
 
+reg		temp_data1_o;
+reg		temp_data2_o_o;
+assign	data_o = temp_data1_o;
+assign	data_o = temp_data2_o;
+
+always @ (*)
+begin
+		case(fu_i)
+			2'b00:	temp_data1_o = data1_i;
+					temp_data2_o = data1_i;
+			2'b01:	temp_data1_o = data2_i;
+					temp_data2_o = data2_i;
+			2'b10:	temp_data1_o = data3_i;
+					temp_data2_o = data3_i;
+			default:
+			
+endcase
+end
+
 endmodule
