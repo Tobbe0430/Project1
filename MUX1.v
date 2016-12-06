c92a2061b32e0047465cccd0d8344edd4f6f96cb
Ericka Bastías 柏藝安 <b03902117@ntu.edu.tw>
2016-12-06 21:27:21 +0800

    mux1 and 2 bases

---
+++ MUX1.v
@@ -0,0 +1,17 @@
+module MUX1
+(
+	and1_i;
+	and2_i;
+	data1_i;
+	data2_i;
+	data_o;
+	andresult_o;
+);
+
+input			and1_i, and2_i;
+input	[31:0]	data1_i,data2_i;
+output	[31:0]	data_o;
+output			andresult_o;
+
+
+endmodule
---
+++ MUX2.v
@@ -0,0 +1,16 @@
+module MUX2
+(
+	data1_i;
+	data2_i;
+	control_i;
+	data_o;
+);
+
+input	[31:0]	data1_i,data2_i;
+input			control_i;
+output	[32:0]	data_o;
+
+assign data_o = (control_i == 0)? (data1_i):
+				data2_i;
+				
+endmodule
