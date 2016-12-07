6ef408182a9d3b8715a076ce9375977094f11f04
Tobbe0430 <tobbe0430@hotmail.com>
2016-12-07 20:02:24 +0800

    FU update

--- FU.v
+++ FU.v
@@ -21,13 +21,13 @@ reg [1:0] mux6ctrl, mux7ctrl;
 
 always @ (*)
 begin
-	if (writeaddr1_i != 32'b0 & writeaddr1_i == rsaddr_i & wb1_i[1] == 1'b01)
+	if (writeaddr1_i != 32'b0 & writeaddr1_i == rsaddr_i & wb1_i[1] == 1'b1)
 		mux6ctrl = 2'b10;
-	else if (writeaddr1_i != 32'b0 & writeaddr1_i == rsaddr_i & wb1_i[1] == 1'b01)
+	else if (writeaddr1_i != 32'b0 & writeaddr1_i == rsaddr_i & wb1_i[1] == 1'b1)
 		mux7ctrl = 2'b10;
-	else if (writeaddr2_i != 32'b0 & writeaddr2_i == rsaddr_i & wb2_i == 1'b01)
+	else if (writeaddr2_i != 32'b0 & writeaddr2_i == rsaddr_i & wb2_i == 1'b1)
 		mux6ctrl = 2'b01;
-	else if (writeaddr2_i != 32'b0 & writeaddr2_i == rtaddr_i & wb2_i == 1'b01)
+	else if (writeaddr2_i != 32'b0 & writeaddr2_i == rtaddr_i & wb2_i == 1'b1)
 		mux7ctrl = 2'b01;
 	else 
 		mux6ctrl = 2'b00;
