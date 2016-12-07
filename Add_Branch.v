babd61183543424a2c6f1e8710807063d702734c
Tobbe0430 <tobbe0430@hotmail.com>
2016-12-07 18:54:23 +0800

    minor update

--- Add_Branch.v
+++ Add_Branch.v
@@ -1,8 +1,8 @@
 module Add_Branch
 (
-	.data1_i,			//32 bits, need shift left 2
-	.data2_i,	 		//32 bits, inst addr
-	.data_o
+	data1_i,			//32 bits, need shift left 2
+	data2_i,	 		//32 bits, inst addr
+	data_o
 );
 
 input [31:0]	data1_i;
