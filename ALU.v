9790d1ab15237ad9516debb39a44959cdb42d2c1
Tobbe0430 <tobbe0430@hotmail.com>
2016-12-07 20:19:19 +0800

    some corrections

--- ALU.v
+++ ALU.v
@@ -2,25 +2,22 @@ module ALU
 (
 	data1_i,
 	data2_i,
-	ALUCtrl_i,
+	aluctrl_i,
 	data_o,
-	Zero_o
 );
 
 input 	[31:0] 	data1_i, data2_i;
-input	[2:0] 	ALUCtrl_i;
+input	[2:0] 	aluctrl_i;
 output 	[31:0] 	data_o;
-output 		  	Zero_o;
 
-reg 			temp_Zero_o;
-assign 			Zero_o = temp_Zero_o;
+
 reg  	[31:0]	temp_data_o;
 assign 			data_o = temp_data_o;
 
 
 always @ (*)
 begin
-		case (ALUCtrl_i)
+		case (aluctrl_i)
 			3'b001: temp_data_o = data1_i+data2_i;
 			3'b010: temp_data_o = data1_i-data2_i;
 			3'b011: temp_data_o = data1_i&data2_i;
@@ -28,8 +25,6 @@ begin
 			3'b101: temp_data_o = data1_i*data2_i;
 			default: temp_data_o = 32'b0;
 		endcase
-		temp_Zero_o = (temp_data_o == 0)? (1):
-				0;
 end
 					
 				
--- ALU_Control.v
+++ ALU_Control.v
@@ -1,29 +1,29 @@
 module ALU_Control
 (
 	funct_i,
-	ALUOp_i,
-	ALUCtrl_o
+	aluop_i,
+	aluctrl_o
 );
 
 input 	[5:0] 	funct_i;
-input 	[1:0] 	ALUOp_i;
-output 	[2:0] 	ALUCtrl_o;
+input 	[1:0] 	aluop_i;
+output 	[2:0] 	aluctrl_o;
 
-reg 	[2:0] 	temp_ALUCtrl_o;
-assign ALUCtrl_o = temp_ALUCtrl_o;
+reg 	[2:0] 	temp_aluctrl_o;
+assign aluctrl_o = temp_aluctrl_o;
 
-always @ (funct_i or ALUOp_i)
+always @ (funct_i or aluop_i)
 begin
-	if (ALUOp_i == 2'b00)
-		temp_ALUCtrl_o = 3'b001;
+	if (aluop_i == 2'b00)
+		temp_aluctrl_o = 3'b001;
 	else
 		case (funct_i)
-			6'b100000: temp_ALUCtrl_o = 3'b001;
-			6'b100010: temp_ALUCtrl_o = 3'b010;
-			6'b100100: temp_ALUCtrl_o = 3'b011;
-			6'b100101: temp_ALUCtrl_o = 3'b100;
-			6'b011000: temp_ALUCtrl_o = 3'b101;
-			default: temp_ALUCtrl_o = 3'b000;
+			6'b100000: temp_aluctrl_o = 3'b001;
+			6'b100010: temp_aluctrl_o = 3'b010;
+			6'b100100: temp_aluctrl_o = 3'b011;
+			6'b100101: temp_aluctrl_o = 3'b100;
+			6'b011000: temp_aluctrl_o = 3'b101;
+			default: temp_aluctrl_o = 3'b000;
 		endcase
 end
 					
