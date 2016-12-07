5a39931deb0c5dfbbe6c7ec5cdf62cf06d4f53af
Tobbe0430 <tobbe0430@hotmail.com>
2016-12-07 18:52:33 +0800

    minor updates

--- MUX8.v
+++ MUX8.v
@@ -1,11 +1,11 @@
 module MUX8
 (
-	.data1_i,				//9  bits, control stuff
-	.data2_i,				//9  bits, all zero
-	.hd_i,					//1  bit, hazard or no hazard
-	.wb_o,					//2  bits, control input
-	.mem_o,					//3  bits, control input
-	.ex_o					//4  bits, control input
+	data1_i,				//9  bits, control stuff
+	data2_i,				//9  bits, all zero
+	hd_i,					//1  bit, hazard or no hazard
+	wb_o,					//2  bits, control input
+	mem_o,					//3  bits, control input
+	ex_o					//4  bits, control input
 );
 
 input	[8:0]	data1_i;
