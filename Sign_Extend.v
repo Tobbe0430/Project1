612ed3f079cc1f7857c50d2a61b410d86f80e2e3
griscarl <gris4carl@hotmail.com>
2016-12-09 11:33:46 +0800

    signextend

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -1,14 +1,22 @@
 module Sign_Extend
 (
-	data_i,
-	data_o
+	// data_i,
+	// data_o
+	if_id_i,
+	id_ex_o,
+	b_add_o
 );
 
-input 	[15:0] 	data_i;
-output 	[31:0] 	data_o;
+input 	[15:0] 	if_id_i;
+output 	[31:0] 	id_ex_o;
+output	[31:0]	b_add_o;
 
-assign data_o[31:16] = (data_i[15] == 1'b1)? (16'b1111111111111111):
+assign id_ex_o[31:16] = (if_id_i[15] == 1'b1)? (16'b1111111111111111):
 					   16'b0000000000000000;
-assign data_o[15:0]  = data_i;
+assign id_ex_o[15:0]  = if_id_i;
+
+assign b_add_o[31:16] = (if_id_i[15] == 1'b1)? (16'b1111111111111111):
+					   16'b0000000000000000;
+assign b_add_o[15:0]  = if_id_i;
 
 endmodule
