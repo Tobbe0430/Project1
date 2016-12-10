0863a760f0a764e7af6b150ea10cf3d95622830c
griscarl <gris4carl@hotmail.com>
2016-12-10 16:23:44 +0800

    DATA MEMEORY

--- Data_Memory.v
+++ Data_Memory.v
@@ -11,7 +11,7 @@ input 			memread_i, memwrite_i;
 input	[31:0]	memaddr_i, writedata_i;
 output	[31:0]	memdata_o;
 
-reg		[31:0]	memory		[0:31];
+reg		[7:0]	memory		[0:31];
 reg		[31:0]  temp_memdata_o;
 
 assign  memdata_o = temp_memdata_o; //Unsure about this one
@@ -20,12 +20,19 @@ assign  memdata_o = temp_memdata_o; //Unsure about this one
 always@(*) 
 if(memwrite_i)
 begin	
-	memory[memaddr_i] <=  writedata_i;
+	memory[memaddr_i] <=  writedata_i[7:0];
+	memory[memaddr_i+1] <=  writedata_i[15:8];
+	memory[memaddr_i+2] <=  writedata_i[23:16];
+	memory[memaddr_i+3] <=  writedata_i[31:24];
 end
+
 always@(*)
 if(memread_i)
 begin
-	temp_memdata_o <= memory[memaddr_i];
+	temp_memdata_o[7:0] <= memory[memaddr_i];
+	temp_memdata_o[15:8] <= memory[memaddr_i+1];
+	temp_memdata_o[23:16] <= memory[memaddr_i+2];
+	temp_memdata_o[31:24] <= memory[memaddr_i+3];
 end
 
 endmodule 
