b86248b088f3c5cab73960daae3eb1b5d7f8a9b0
griscarl <gris4carl@hotmail.com>
2016-12-10 17:08:10 +0800

    fu fitta

--- FU.v
+++ FU.v
@@ -2,8 +2,8 @@ module FU
 (
 	rsaddr_i,
 	rtaddr_i,
-	writeaddr1_i,
-	writeaddr2_i,
+	writeaddr1_i, //EX_MEM.writeaddr
+	writeaddr2_i, //MEM_WB.writeaddr
 	wb1_i,
 	wb2_i,
 	mux6_o,
@@ -24,13 +24,13 @@ assign mux7_o = mux7ctrl;
 
 always @ (*)
 begin
-	if ((writeaddr1_i != 32'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[1] == 1'b1))
+	if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[1] == 1'b1))
 		mux6ctrl = 2'b10;
-	else if ((writeaddr1_i != 32'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[1] == 1'b1))
+	else if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[1] == 1'b1))
 		mux7ctrl = 2'b10;
-	else if ((writeaddr2_i != 32'b0) & (writeaddr2_i == rsaddr_i) & (wb2_i == 1'b1))
+	else if ((writeaddr2_i != 5'b0) & (writeaddr2_i == rsaddr_i) & (wb2_i == 1'b1))
 		mux6ctrl = 2'b01;
-	else if ((writeaddr2_i != 32'b0) & (writeaddr2_i == rtaddr_i) & (wb2_i == 1'b1))
+	else if ((writeaddr2_i != 5'b0) & (writeaddr2_i == rtaddr_i) & (wb2_i == 1'b1))
 		mux7ctrl = 2'b01;
 	else 
 		mux6ctrl = 2'b00;
