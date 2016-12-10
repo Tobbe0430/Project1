b20ef7c0aa700aeec327d01a6d2ed70f598317f4
griscarl <gris4carl@hotmail.com>
2016-12-10 11:48:44 +0800

    Fixed some stuff in PC

--- PC.v
+++ PC.v
@@ -18,24 +18,23 @@ input   [31:0]  	pc_i;
 output  [31:0]  	pc1_o, pc2_o;
 
 // Wires & Registers NOT WRITTEN BY US
-reg     [31:0]      pc1_o;
-reg     [31:0]      pc2_o;
+reg     [31:0]      pc;
+
+assign pc1_o = pc;
+assign pc2_o = pc;
 
 always@(posedge clk_i) begin
     if (hd_i) begin
-        pc1_o <= 32'b0;
-		pc2_o <= 32'b0;
-    end
+        pc <= 32'b0;		//Tvek om det ska vara nollor här, tror	//Det borde betyda att man börjar om från början, det vill vi inte.
+    end					
     else begin
         if(start_i)
 			begin
-            pc1_o <= pc_i;
-			pc2_o <= pc_i;
+            pc <= 32'b0; //här ska det nog vara nollor så vi börjar //från första instructionen
 			end
         else
 			begin
-            pc1_o <= pc1_o;
-			pc2_o <= pc2_o;
+            pc <= pc_i;
 			end
     end
 end
