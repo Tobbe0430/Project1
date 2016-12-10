722b6a57b934c0a897f96c182e2693821c1b5a89
Tobbe0430 <tobbe0430@hotmail.com>
2016-12-10 12:34:23 +0800

    bajs

--- PC.v
+++ PC.v
@@ -19,6 +19,7 @@ output  [31:0]  	pc1_o, pc2_o;
 
 // Wires & Registers NOT WRITTEN BY US
 reg     [31:0]      pc;
+reg 				startdisable;//TOBBE
 
 assign pc1_o = pc;
 assign pc2_o = pc;
@@ -28,9 +29,10 @@ always@(posedge clk_i) begin
         pc <= 32'b0;		//Tvek om det ska vara nollor här, tror	//Det borde betyda att man börjar om från början, det vill vi inte.
     end					
     else begin
-        if(start_i)
+        if((start_i) & (startdisable == 1'b0))//TOBBE
 			begin
             pc <= 32'b0; //här ska det nog vara nollor så vi börjar //från första instructionen
+			startdisable <= 1'b1;//TOBBE
 			end
         else
 			begin
