67610429282852e472ee793035f5a5530c5f4d2c
griscarl <gris4carl@hotmail.com>
2016-12-10 17:30:31 +0800

    kukfitta fFU

--- FU.v
+++ FU.v
@@ -24,17 +24,19 @@ assign mux7_o = mux7ctrl;
 
 always @ (*)
 begin
-	if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[1] == 1'b1))
+	if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rsaddr_i) & (wb1_i[0] == 1'b1))
 		mux6ctrl = 2'b10;
-	else if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rtaddr_i) & (wb1_i[1] == 1'b1))
+	else if ((writeaddr1_i != 5'b0) & (writeaddr1_i == rtaddr_i) & (wb1_i[0] == 1'b1))
 		mux7ctrl = 2'b10;
 	else if ((writeaddr2_i != 5'b0) & (writeaddr2_i == rsaddr_i) & (wb2_i == 1'b1))
 		mux6ctrl = 2'b01;
 	else if ((writeaddr2_i != 5'b0) & (writeaddr2_i == rtaddr_i) & (wb2_i == 1'b1))
 		mux7ctrl = 2'b01;
 	else 
+		begin
 		mux6ctrl = 2'b00;
 		mux7ctrl = 2'b00;
+		end
 end
 
 endmodule
